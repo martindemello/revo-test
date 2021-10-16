1aefdb5f0c7382e5596d1dc7e6a94ed1ee95c6a3
Martin DeMello <martindemello@gmail.com>
2021-10-16 04:51:21 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-grid theme="compact" :source="rows" :columns="columns" />
+</template>
+
+<script>
+import VGrid from "@revolist/vue3-datagrid";
+export default {
+  name: "App",
+  data() {
+    return {
+      columns: [
+        {
+          name: "Birth",
+          prop: "birthdate",
+          columnType: "date",
+          size: 150,
+        },
+        {
+          prop: "name",
+          name: "First",
+        },
+        {
+          prop: "details",
+          name: "Second",
+        },
+      ],
+      rows: [
+        {
+          birthdate: "2022-08-24",
+          name: "1",
+          details: "Item 1",
+        },
+        {
+          birthdate: "2022-08-24",
+          name: "2",
+          details: "Item 2",
+        },
+      ],
+    };
+  },
+  components: {
+    VGrid,
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 700px;
+  width: 500px;
+}
+revo-grid {
+  height: 100%;
+}
+</style>
